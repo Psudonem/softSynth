5a86e0cfa0f4056a77caa6912a12e368f9113270
Psudonem <jaseystudios@gmail.com>
2023-03-22 17:21:19 -0400

    update

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import mido
 
 class instrument:
       def __init__(self,func):
-            self.WAVEDATA = ''
             self.function=func
+      def wave(self,t):
+            y = eval(func)
+            return y
 
 class softSynth:
       def __init__(self):
             self.BITRATE = 24000
+            self.WAVEDATA = ''
             self.instruments=[]
 
       
